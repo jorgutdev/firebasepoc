097e55166fe6c063704fdd814816e6a43f78db32
Jorge Gutierrez Parejo <jgp@tetralog.es>
2018-04-06 14:45:24 +0200

    WIP

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import React from "react";
+import { bindActionCreators, compose } from "redux";
+import { connect } from "react-redux";
+import { withFirebase, isLoaded, isEmpty } from "react-redux-firebase";
 
 export class Main extends React.Component {
   constructor(props) {
@@ -9,17 +10,22 @@ export class Main extends React.Component {
   }
 
   render() {
-    console.log(this.props);
+    console.log(this.props.firebase);
     return <div>{JSON.stringify(this.props)}</div>;
   }
 }
 
 const mapStateToProps = state => ({
-  firebase: state,
+  firebase: state
 });
 
 const mapDispatchToProps = dispatch => ({
-  actions: bindActionCreators({}, dispatch),
+  actions: bindActionCreators({}, dispatch)
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Main);
+export default compose(
+  withFirebase([
+    "" // { path: '/todos' } // object notation
+  ]),
+  connect(mapStateToProps, mapDispatchToProps)
+)(Main);
